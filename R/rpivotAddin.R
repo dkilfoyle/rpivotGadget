356be20926ec8a8f64c2b2cde9d8480a70357124
dkilfoyle <dkilfoyle@xtra.co.nz>
2016-03-20 12:05:10 +1300

    Capture pivot refresh data

--- R/rpivotAddin.R
+++ R/rpivotAddin.R
@@ -3,10 +3,20 @@ library(rpivotTable)
 library(DT)
 library(miniUI)
 
+list_to_string <- function(obj, listname) {
+  if (is.null(names(obj))) {
+    paste(listname, "[[", seq_along(obj), "]] = ", obj,
+      sep = "", collapse = "\n")
+  } else {
+    paste(listname, "$", names(obj), " = ", obj,
+      sep = "", collapse = "\n")
+  }
+}
+
 rpivotAddin <- function() {
 
   ui <- miniPage(
-    gadgetTitleBar("Shiny gadget example"),
+    gadgetTitleBar("Pivot Table Gadget"),
     miniTabstripPanel(
       miniTabPanel("Pivot", icon = icon("sliders"),
         miniContentPanel(
@@ -16,6 +26,11 @@ rpivotAddin <- function() {
       miniTabPanel("Data", icon = icon("table"),
         miniContentPanel(
           DT::dataTableOutput("table")
+        )
+      ),
+      miniTabPanel("PivotData", icon = icon("table"),
+        miniContentPanel(
+          verbatimTextOutput("pivotRefresh")
         )
       )
     )
@@ -29,6 +44,21 @@ rpivotAddin <- function() {
 
     output$table <- DT::renderDataTable({
       diamonds
+    })
+
+    output$pivotRefresh <- renderText({
+
+      cnames <- list("cols","rows","vals", "exclusions","inclusions", "aggregatorName", "rendererName")
+      # Apply a function to all keys, to get corresponding values
+      allvalues <- lapply(cnames, function(name) {
+        item <- input$myPivotData[[name]]
+        if (is.list(item)) {
+          list_to_string(item, name)
+        } else {
+          paste(name, item, sep=" = ")
+        }
+      })
+      paste(allvalues, collapse = "\n")
     })
 
     observeEvent(input$done, {
