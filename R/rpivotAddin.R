87e1013fbe270d0fdce8bda6ec0abfae7ecdb869
dkilfoyle <dkilfoyle@xtra.co.nz>
2016-03-21 22:56:05 +1300

    miniButtonBlock and 100% code height

--- R/rpivotAddin.R
+++ R/rpivotAddin.R
@@ -8,43 +8,57 @@ library(rstudioapi)
 
 list_to_string <- function(obj, listname) {
   if (is.null(names(obj))) {
-    paste(listname, "[[", seq_along(obj), "]] = ", obj,
-      sep = "", collapse = "\n")
+    paste(listname,
+      "[[",
+      seq_along(obj),
+      "]] = ",
+      obj,
+      sep = "",
+      collapse = "\n")
   } else {
-    paste(listname, "$", names(obj), " = ", obj,
-      sep = "", collapse = "\n")
+    paste(listname,
+      "$",
+      names(obj),
+      " = ",
+      obj,
+      sep = "",
+      collapse = "\n")
   }
 }
 
-tmplSummariseN = '
+tmplSummariseN =
+  '# selected = {{groupby}}
 {{df}} %>%
-  group_by({{groupby}}) %>%
-  summarise(n=n())
-'
+group_by({{groupby}}) %>%
+summarise(n=n())'
 
 rpivotAddin <- function() {
-
   ui <- miniPage(
     gadgetTitleBar("Pivot Table Gadget"),
 
-    miniTabstripPanel(id="gadgetTabstrip",
+    miniTabstripPanel(
+      id = "gadgetTabstrip",
 
-      miniTabPanel("Pivot", icon = icon("table"),
-        miniContentPanel(
-          rpivotTableOutput("mypivot")
-        )
+      miniTabPanel(
+        "Pivot",
+        icon = icon("table"),
+        miniContentPanel(rpivotTableOutput("mypivot"))
       ),
 
-      miniTabPanel("Setup", icon = icon("bars"),
+      miniTabPanel(
+        "Setup",
+        icon = icon("bars"),
         miniContentPanel(
-          fillPage(
-            fillCol(flex=c(NA,NA,NA,NA),
-              selectInput("dataset", "Dataframe:", choices=getDataFrames()),
-              verbatimTextOutput("pivotRefresh"),
-              aceEditor("rcode", "# R code will appear here", mode="r" ),
-              fillRow(actionButton("code2clipboard", "Copy to clipboard", icon("clipboard"), width="100%"), actionButton("code2console", "Execute in console", icon("play"), width="100%"))
-            )
+          fillCol(
+            flex = c(NA, NA, 1),
+            selectInput("dataset", "Dataframe:", choices = getDataFrames()),
+            verbatimTextOutput("pivotRefresh"),
+            aceEditor("rcode", "# R code will appear here", mode = "r", height="100%")
           )
+        ),
+        miniButtonBlock(
+          actionButton("code2clipboard", "Copy to clipboard", icon("clipboard")),
+          actionButton("code2console", "Execute in console", icon("play"))
         )
       )
 
@@ -52,25 +66,37 @@ rpivotAddin <- function() {
   )
 
   server <- function(input, output, session) {
-
     getSelectedDF <- reactive({
-      eval(parse(text=input$dataset))
+      eval(parse(text = input$dataset))
     })
 
     output$mypivot <- renderRpivotTable({
-      rpivotTable(getSelectedDF(), onRefresh=htmlwidgets::JS("function(config) { Shiny.onInputChange('myPivotData', config); }"))
+      rpivotTable(
+        getSelectedDF(),
+        onRefresh = htmlwidgets::JS(
+          "function(config) { Shiny.onInputChange('myPivotData', config); }"
+        )
+      )
       # updateTabsetPanel(session, "gadgetTabstrip", selected="pivot")
     })
 
     output$pivotRefresh <- renderText({
-
-      cnames <- list("cols","rows","vals", "exclusions","inclusions", "aggregatorName", "rendererName")
+      cnames <-
+        list(
+          "cols",
+          "rows",
+          "vals",
+          "exclusions",
+          "inclusions",
+          "aggregatorName",
+          "rendererName"
+        )
       allvalues <- lapply(cnames, function(name) {
         item <- input$myPivotData[[name]]
         if (is.list(item)) {
           list_to_string(item, name)
         } else {
-          paste(name, item, sep=" = ")
+          paste(name, item, sep = " = ")
         }
       })
       paste(allvalues, collapse = "\n")
@@ -85,9 +111,18 @@ rpivotAddin <- function() {
     })
 
     observeEvent(input$myPivotData, {
-      if (length(input$myPivotData[["rows"]])==1 & input$myPivotData[["aggregatorName"]] =="Count") {
+      if (length(input$myPivotData[["rows"]]) == 1 &
+          input$myPivotData[["aggregatorName"]] == "Count") {
         # if rendererName
-        updateAceEditor(session, "rcode", whisker.render(tmplSummariseN, list(df = input$dataset, groupby = input$myPivotData[["rows"]][1])))
+        updateAceEditor(session,
+          "rcode",
+          whisker.render(
+            tmplSummariseN,
+            list(
+              df = input$dataset,
+              groupby = input$myPivotData[["rows"]][1]
+            )
+          ))
       }
     })
 
@@ -98,4 +133,3 @@ rpivotAddin <- function() {
 
   runGadget(shinyApp(ui, server), viewer = paneViewer())
 }
-
