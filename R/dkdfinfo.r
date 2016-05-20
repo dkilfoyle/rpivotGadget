879df044f5ee7a10b51e1a90005f3b03afca8299
dkilfoyle <dkilfoyle@xtra.co.nz>
2016-05-21 09:55:51 +1200

    more concise getdataframes

--- R/dkdfinfo.r
+++ R/dkdfinfo.r
@@ -1,6 +1,7 @@
 getDataFrames = function()
 {
-  ls(envir=.GlobalEnv)[sapply(ls(envir=.GlobalEnv), function(x){is.data.frame(get(x))})]
+  # ls(envir=.GlobalEnv)[sapply(ls(envir=.GlobalEnv), function(x){is.data.frame(get(x))})]
+  names(which(sapply(.GlobalEnv, is.data.frame)))
 }
 
 getdfinfo = function(dfn)
@@ -12,14 +13,14 @@ getdfinfo = function(dfn)
   fields.factor = fields[fields.type %in% c("factor", "binaryfactor")]
   fields.logical = fields[fields.type %in% c("logical","binaryfactor")]
   fields.date = fields[fields.type %in% c("date")]
-  
+
   getdfinfo = list(
     numerics = list(name=as.vector(fields.numeric),
       mean=sapply(fields.numeric, function(x) { round(mean(mydf[,x], na.rm=T),2) } ),
       min =sapply(fields.numeric, function(x) { min(mydf[,x], na.rm=T)}),
       max =sapply(fields.numeric, function(x) { max(mydf[,x], na.rm=T) }),
       NAs =sapply(fields.numeric, function(x) { sum(is.na(mydf[,x])) }),
-      spark =sapply(fields.numeric, function(x) { 
+      spark =sapply(fields.numeric, function(x) {
         boxstats = boxplot.stats(mydf[,x], do.out=T)
         paste(span(class="sparkline", values=paste(boxstats$stats, sep="", collapse=",")))
         })
@@ -34,7 +35,7 @@ getdfinfo = function(dfn)
           levels(xx) = c(0,1)
           xx=as.numeric(xx)
         }
-        mean(xx, na.rm=T)    
+        mean(xx, na.rm=T)
       })),
     dates = list(name=as.vector(fields.date),
       min = sapply(fields.date, function(x) { min(mydf[,x], na.rm=T) }),
@@ -48,7 +49,7 @@ getNumerics = function(dfn) {
   fields = names(mydf)
   fields.type = sapply(fields, getVectorType, mydf)
   fields.numeric = fields[fields.type %in% c("numeric","integer","double", "logical")]
-  
+
   cbind(label=sapply(fields.numeric, function(x) { sprintf("%s/Range: %.0f-%.0f | NAs: %.0f", x, min(mydf[,x],na.rm=T), max(mydf[,x],na.rm=T), sum(is.na(mydf[,x]))) }))
 }
 
@@ -57,7 +58,7 @@ getFactors = function(dfn) {
   fields = names(mydf)
   fields.type = sapply(fields, getVectorType, mydf)
   fields.factor = fields[fields.type %in% c("factor", "binaryfactor")]
-  
+
   cbind(label=sapply(fields.factor, function(x) { sprintf("%s/nlevels=%.0f NAs=%.0f", x, nlevels(mydf[,x]), sum(is.na(mydf[,x]))) }))
 }
 
@@ -66,7 +67,7 @@ getDates = function(dfn) {
   fields = names(mydf)
   fields.type = sapply(fields, getVectorType, mydf)
   fields.date = fields[fields.type %in% c("date")]
-  
+
   cbind(label=sapply(fields.date, function(x) { sprintf("%s/min=%s max=%s NAs=%.0f", x, min(mydf[,x], na.rm=T), max(mydf[,x], na.rm=T), sum(is.na(mydf[,x]))) }))
 }
 
@@ -81,8 +82,8 @@ getVectorType = function(field, mydf)
       x="logical"
     }
   }
-  if (is.factor(mydf[,field])) { 
-    x = "factor" 
+  if (is.factor(mydf[,field])) {
+    x = "factor"
     if (nlevels(mydf[,field]) == 2)
     {
       x="binaryfactor"
